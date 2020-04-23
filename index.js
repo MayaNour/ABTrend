6be9ea7fb71fc83fa898bc3adfc56d0593916759
Maya <maya.noorallah.89@gmail.com>
2020-04-24 01:40:50 +0300

    change in style in showing product photos part

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ function showImage(imgs) {
     }
     document.getElementById(tabName).style.display = "block";
   }
+
+  function openDropList() {
+    document.getElementById("sizeDropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function selectItem(optionValue){
+  document.querySelector("#sizebtn").textContent = optionValue.text;
+}
