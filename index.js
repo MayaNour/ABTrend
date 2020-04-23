84dc088967f1d7f40bbd51e9cc5fa69c2bcc7d99
Maya <maya.noorallah.89@gmail.com>
2020-04-23 17:27:36 +0300

    add rules part and other categories part

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function showImage(imgs) {
     expandImg.src = imgs.src;
     expandImg.parentElement.style.display = "block";
   }
+
+  function openTab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
